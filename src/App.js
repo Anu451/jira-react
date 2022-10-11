45163d840381e084e105275fffb5355e7b8217e2
Anushka D M <rajeshoffice07@example.com>
2022-10-11 16:30:18 +0530

    path

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import SideBar from './components/SideBar';
+import Path from './components/Path';
 
 function App() {
-  return (
-    <div className="App">
-    <NavBar></NavBar>
-    <SideBar></SideBar>
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavBar></NavBar>
+			<div className="content">
+				<SideBar></SideBar>
+				<div className="content-path">
+					<Path></Path>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Path.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './path.css'
+
+function Path() {
+	return (
+		<div className="path">
+			<ul >
+				<li>Project</li>
+				<li>/</li>
+				<li>Test</li>
+				<li>/</li>
+				{/* <li><img class="content-path-logo" src="/logo2.png" alt=""><span>Test-1</span>/</li> */}
+			</ul>
+		</div>
+	);
+}
+
+export default Path
